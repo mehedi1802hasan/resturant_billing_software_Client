8f0adb6930486834704b191a3349693896c6a1f5
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-09-24 18:04:04 +0600

    worked item section and implement toaster

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 const Home = () => {
   const [menu,setMenu]=useState([]);
   const [cart, setCart] = useState([]);
@@ -14,6 +16,7 @@ const Home = () => {
   const dessert =menu.filter(item=>item.category === 'dessert');
   const soup =menu.filter(item=>item.category === 'soup');
   const handleAddToCard =(item)=>{
+    toast('add food on the ADD to Card')
     const updatedCart = [...cart, item];
     setCart(updatedCart);
     localStorage.setItem('cart', JSON.stringify(updatedCart));
@@ -22,26 +25,26 @@ const Home = () => {
   }
     return (
     <div>
-       <Tabs className='p-20 '>
-    <TabList className=' flex gap-14 justify-center items-center '>
-      <Tab  className='border-2  border-black px-10 py-4 rounded-xl hover:bg-yellow-500'>Pizza</Tab>
-      <Tab  className='border-2 border-black px-10 py-4 rounded-xl  hover:bg-yellow-500'>Drinks</Tab>
-      <Tab  className='border-2 border-black px-10 py-4 rounded-xl  hover:bg-yellow-500'>Dessert</Tab>
-      <Tab  className='border-2 border-black px-10 py-4 rounded-xl  hover:bg-yellow-500 '>Soup</Tab>
+       <Tabs className='p-20  '>
+    <TabList className=' p-10 rounded flex justify-center items-center  gap-9'>
+      <Tab  className='  border-b-2  border-pink-500 px-10 py-4 rounded-xl hover:bg-pink-400'>Pizza</Tab>
+      <Tab  className=' border-b-2 border-pink-500 px-10 py-4 rounded-xl  hover:bg-pink-400'>Drinks</Tab>
+      <Tab  className=' border-b-2 border-pink-500 px-10 py-4 rounded-xl  hover:bg-pink-400'>Dessert</Tab>
+      <Tab  className=' border-b-2 border-pink-500 px-10 py-4 rounded-xl  hover:bg-pink-400 '>Soup</Tab>
     </TabList>
 
-    <TabPanel className='mt-14'>
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-16 '>
+    <TabPanel className='mt-5'>
+    <div className='grid grid-cols-1 md:grid-cols-3 gap-12 '>
         {
             pizza.map(item=>
-          <div key={item._id} className="card card-compact  w-72 bg-base-100 shadow-xl ">
+          <div key={item._id} className="card card-compact rounded-2xl  w-[330px] bg-base-100 shadow-xl ">
   <figure><img className='h-52 w-full' src={item.image} alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">{item.name}</h2>
   
     <div className="card-actions flex items-center justify-end">
     <p>Price: ${item.price}</p>
-      <button onClick={()=>handleAddToCard(item)} className="btn btn-primary">Buy Now</button>
+      <button onClick={()=>handleAddToCard(item)} className="btn btn-outline btn-secondary">Buy Now</button>
     </div>
   </div>
 </div>
@@ -50,17 +53,17 @@ const Home = () => {
     </div>
     </TabPanel>
     <TabPanel className='mt-14'>
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-24 '>
+    <div className='grid grid-cols-1 md:grid-cols-3 gap-12 '>
         {
             drinks.map(item=>
-             <div key={item._id} className="card card-compact  w-72 bg-base-100 shadow-xl ">
+             <div key={item._id} className="card card-compact  rounded-2xl  w-[330px] bg-base-100 shadow-xl ">
   <figure><img className='h-52 w-full' src={item.image} alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">{item.name}</h2>
   
     <div className="card-actions flex items-center justify-end">
     <p>Price: ${item.price}</p>
-      <button onClick={()=>handleAddToCard(item)} className="btn btn-primary">Buy Now</button>
+      <button onClick={()=>handleAddToCard(item)} className="btn btn-outline btn-secondary">Buy Now</button>
     </div>
   </div>
 </div>
@@ -69,17 +72,17 @@ const Home = () => {
     </div>
     </TabPanel>
     <TabPanel className='mt-14'>
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-24 '>
+    <div className='grid grid-cols-1 md:grid-cols-3 gap-12 '>
         {
             dessert.map(item=>
-             <div key={item._id} className="card card-compact  w-72 bg-base-100 shadow-xl ">
+             <div key={item._id} className="card card-compact rounded-2xl  w-[330px] bg-base-100 shadow-xl ">
   <figure><img className='h-52 w-full' src={item.image} alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">{item.name}</h2>
   
     <div className="card-actions flex items-center justify-end">
     <p>Price: ${item.price}</p>
-      <button onClick={()=>handleAddToCard(item)} className="btn btn-primary">Buy Now</button>
+      <button onClick={()=>handleAddToCard(item)} className="btn btn-outline btn-secondary">Buy Now</button>
     </div>
   </div>
 </div>
@@ -88,18 +91,20 @@ const Home = () => {
     </div>
     </TabPanel>
     <TabPanel className='mt-14'>
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-16 '>
+    <div className='grid grid-cols-1 md:grid-cols-3 gap-12 '>
         {
             soup.map(item=>
-          <div key={item._id} className="card card-compact  w-72 bg-base-100 shadow-xl ">
+          <div key={item._id} className="card card-compact  rounded-2xl  w-[330px] bg-base-100 shadow-xl ">
   <figure><img className='h-52 w-full' src={item.image} alt="Shoes" /></figure>
   <div className="card-body">
     <h2 className="card-title">{item.name}</h2>
   
     <div className="card-actions flex items-center justify-end">
     <p>Price: ${item.price}</p>
-      <button onClick={()=>handleAddToCard(item)} className="btn btn-primary">Add-Card</button>
+      <button onClick={()=>handleAddToCard(item)} className="btn btn-outline btn-secondary">Add-Card</button>
     </div>
+   
+
   </div>
 </div>
             )
@@ -107,6 +112,7 @@ const Home = () => {
     </div>
     </TabPanel>
   </Tabs>
+  <ToastContainer/>
     </div>
   );
 };
--- src/Pages/Home/Items.jsx
+++ src/Pages/Home/Items.jsx
@@ -1,36 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import { useHistory } from 'react-router-dom'; // Import useHistory from react-router-dom
 
 const Items = () => {
     const cartData = JSON.parse(localStorage.getItem('cart'));
-    const total=cartData.reduce((sum,item)=>item.price + sum,0);
-    const gst = (total * 0.14).toFixed(2); 
-        const handleDelete = (itemToDelete) => {
-        // Filter out the item to delete from the cartData
-        const updatedCart = cartData.filter((item) => item._id !== itemToDelete._id);
-      
-        // Update the cart data in localStorage
-        localStorage.setItem('cart', JSON.stringify(updatedCart));
-      
+    const customerInfo = JSON.parse(localStorage.getItem('customerInfo'));
+    const [showResults, setShowResults] = useState(false);
+    const [customerName, setCustomerName] = useState('');
+    // const history = useHistory(); // Get the history object for navigation
+
+    const total = cartData.reduce((sum, item) => item.price + sum, 0);
+    const gst = total * 0.14;
+
+    const handleCustomerInfo = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const customerName = form.customerName.value;
+        console.log(customerName);
+        localStorage.setItem('customerInfo', JSON.stringify(customerName));
+
+        setCustomerName('');
+
+        setShowResults(true);
+    };
+
+    const handleDelete = (itemToDelete) => {
+        // ... (your existing code)
+
         // Refresh the component to reflect the updated cart
         window.location.reload();
-      };
+    };
+
+    useEffect(() => {
+        // Clear customerInfo when navigating to another route
+        return () => {
+            localStorage.removeItem('customerInfo');
+        };
+    }, []); // The empty dependency array ensures this effect runs only once on component mount
+
     return (
         <div>
-           <h3 className='text-3xl text-pink-500 font-serif my-16'>  Total AddTOCard Item..{cartData.length}</h3>
-           <div className=''>
-           {
-                cartData.map(item=><div className='flex my-6 gap-12 justify-center items-center'>
-                    <h3><img className='h-16 w-16 rounded-3xl' src={item.image} alt="" /></h3>
-                    <h3>{item.name}</h3>
-                    <p>{item.price}</p>
-                    <button onClick={()=>handleDelete(item)} className='btn btn-outline'> Delete</button>
-                </div>)
-            }
-           </div>
-           <h4 className=' text-3xl font-bold font-serif text-center '>           tax: {gst}
-</h4>
-<h4 className=' text-3xl font-bold font-serif text-center '> total price : {gst + total}</h4>
-
+            <h3 className='text-3xl text-pink-500 font-serif my-16'>  Total AddTOCard Item..{cartData.length}</h3>
+            <div className=''>
+                {cartData.map((item) => (
+                    <div className='flex my-6 gap-12 justify-center items-center' key={item._id}>
+                        <h3><img className='h-16 w-16 rounded-3xl' src={item.image} alt="" /></h3>
+                        <h3>{item.name}</h3>
+                        <p>{item.price}</p>
+                        <button onClick={() => handleDelete(item)} className='btn btn-outline'> Delete</button>
+                    </div>
+                ))}
+            </div>
+            <form onSubmit={handleCustomerInfo}>
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Customer-Name</span>
+                    </label>
+                    <input type="text" name='customerName' placeholder="Enter your Product name" className="input input-bordered  md:w-[400px] p-5 rounded-2xl" required />
+                </div>
+                <button className='btn btn-outline'>Save</button>
+            </form>
+            <h3>{customerInfo}</h3>
+            {showResults && (
+                <>
+                    <h4 className="text-3xl font-bold font-serif text-center">Tax: {gst}</h4>
+                    <h4 className="text-3xl font-bold font-serif text-center">Total Price: {gst + total}</h4>
+                </>
+            )}
         </div>
     );
 };
