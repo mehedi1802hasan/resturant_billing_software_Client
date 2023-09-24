1d41c22c789eca03b8fe83bd1d8cfaf0a95a33f9
mehedi1802hasan <mehedi1802hasan@gmail.com>
2023-09-24 12:56:46 +0600

    fixed total and gst price

--- src/Pages/Home/Items.jsx
+++ src/Pages/Home/Items.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const Items = () => {
     const cartData = JSON.parse(localStorage.getItem('cart'));
-    const handleDelete = (itemToDelete) => {
+    const total=cartData.reduce((sum,item)=>item.price + sum,0);
+    const gst = (total * 0.14).toFixed(2); 
+        const handleDelete = (itemToDelete) => {
         // Filter out the item to delete from the cartData
         const updatedCart = cartData.filter((item) => item._id !== itemToDelete._id);
       
@@ -15,14 +17,19 @@ const Items = () => {
     return (
         <div>
            <h3 className='text-3xl text-pink-500 font-serif my-16'>  Total AddTOCard Item..{cartData.length}</h3>
-            {
-                cartData.map(item=><div className='flex gap-12 justify-center items-center'>
+           <div className=''>
+           {
+                cartData.map(item=><div className='flex my-6 gap-12 justify-center items-center'>
                     <h3><img className='h-16 w-16 rounded-3xl' src={item.image} alt="" /></h3>
                     <h3>{item.name}</h3>
                     <p>{item.price}</p>
                     <button onClick={()=>handleDelete(item)} className='btn btn-outline'> Delete</button>
                 </div>)
             }
+           </div>
+           <h4 className=' text-3xl font-bold font-serif text-center '>           tax: {gst}
+</h4>
+<h4 className=' text-3xl font-bold font-serif text-center '> total price : {gst + total}</h4>
 
         </div>
     );
